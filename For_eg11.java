40054154c7ac6018b1191706aaf4fc1cf28ad417
Rupali More <167845501+RupaSMore@users.noreply.github.com>
2026-05-28 17:09:26 +0530

    Add files via upload

---
+++ For_eg11.java
@@ -0,0 +1,15 @@
+package Ex_May26_Loops;
+
+public class For_eg11 {
+    public static void main(String[] args) {
+     for(int i=0; i<10; i++)
+     {
+         System.out.println("Rupali");
+     }
+     for (int i=1; i<=10; i++)
+     {
+         System.out.println("More");
+     }
+
+    }
+}
